5edf91034fa1b3e4c40bb29f43c3bf8e800e3a79
hamadklz <hamad.saad.alharthi@gmail.com>
2025-12-05 16:46:47 +0300

    Add files via upload

---
+++ AccountContoller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace boat_haraj.Controllers
+{
+    public class AccountContoller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
